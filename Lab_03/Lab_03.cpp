a15a0c9691bb37b917c525a41190493d0b5a80d1
belenbarbed <belenbarbed@gmail.com>
2016-11-23 19:55:10 +0000

    moar

--- Lab_03/Lab_03.cpp
+++ Lab_03/Lab_03.cpp
@@ -17,14 +17,29 @@ using namespace std;
 
 void testClass();
 
+void file_to_vector(vector <Point>& v);
+
 int farthest_point(vector <Point>& a);
 int closest_point(vector <Point>& a);
 
 int member_point_unordsearch(const Point& p, vector <Point>& v);
+int member_point_binsearch(const Point& p, vector <Point>& v);
 
 int main() {
 
-	testClass();
+	vector <Point> v;
+	file_to_vector(v);
+	
+	double x, y;
+	cout << "Enter the point to look for in the vector" << endl;
+	cin >> x >> y;
+	Point p(x, y);
+	
+	if(member_point_unordsearch(p, v) == v.size()){
+		cout << "The point was not found" << endl;
+	} else {
+		cout << "The point is in the file, in index " << member_point_unordsearch(p, v) << endl;
+	}
 
 	return 0;
 }
@@ -49,6 +64,25 @@ void testClass(){
 	p.make_sym();
 	cout << "The symmetric to the point is " << p << endl;
 
+}
+
+void file_to_vector(vector <Point>& v){
+	
+	double x, y;
+
+	ifstream infile;
+	infile.open("inputpoints.txt");
+	if(!infile.is_open()){
+		cout << "Could not open input file." << endl;
+		exit(EXIT_FAILURE);
+	}
+
+	while(infile >> x >> y){
+		Point tmp(x, y);
+		v.push_back(tmp);
+	}
+
+	infile.close();
 }
 
 int farthest_point(vector <Point>& a){
@@ -98,5 +132,11 @@ int member_point_unordsearch(const Point& p, vector <Point>& v){
 	}
 	
 	return v.size();
+	
+}
+
+int member_point_binsearch(const Point& p, vector <Point>& v){
+	
+	
 	
 }
