11ceaacaea836040fbd8985b4a7d7562f4bbfab3
belenbarbed <belenbarbed@gmail.com>
2016-11-28 23:32:48 +0000

    moar shit

--- Lab_06/Lab_06.cpp
+++ Lab_06/Lab_06.cpp
@@ -6,6 +6,8 @@
  */
 
 #include <iostream>
+#include <vector>
+#include <complex> 
  
 #include "impedance_device.hpp"
 
@@ -13,6 +15,57 @@ using namespace std;
 
 int main(){
 	
+	vector<Impedance_Device*> circuit;
+	
+	Impedance_Device *tmp_id;
+	
+	char dc;
+	double tmp_rci;
+	int n_devices;
+	
+	cout << "number of devices: ";
+	cin >> n_devices;
+	
+	for(int i = 0; i < n_devices; i++){
+		
+		cout << "chose the device (r/c/i): ";
+		cin >> dc;
+		
+		if(dc == 'c'){
+			cout << "capacitance (F): ";
+			cin >> tmp_rci;
+			tmp_id = new Capacitor(tmp_rci);
+		}
+		
+		else if(dc == 'i'){
+			cout << "impendance (H): ";
+			cin >> tmp_rci;
+			tmp_id = new Inductor(tmp_rci);
+		}
+		
+		else{
+			cout << "resistance (Ohm): ";
+			cin >> tmp_rci;
+			tmp_id = new Resistor(tmp_rci);
+		}
+		circuit.push_back(tmp_id);
+		
+	}
+	
+	double f;
+	cout << "frequency: ";
+	cin >> f;
+	
+	for(int i = 0; i < n_devices; i++){
+		
+		cout << "impedance " << (i+1) << ": ";
+		cout << circuit[i]->get_impedance(f * 6.28) << " Ohms" << endl;
+		
+	}
+	
+	for(int i = 0; i < n_devices; i++){
+		delete circuit[i];
+	}
 	
 	return 0;
 }
--- Lab_06/impedance_device.hpp
+++ Lab_06/impedance_device.hpp
@@ -5,10 +5,72 @@
  *      Author: bb2115
  */
  
+#include <complex> 
+ 
 #ifndef IMPEDANCE_DEVICE_HPP_
 #define IMPEDANCE_DEVICE_HPP_
 
+class Impedance_Device{
+	
+public:
 
+	// destructor
+	virtual ~Impedance_Device() {}
 
+	// methods
+	virtual std::complex<double> get_impedance(double omega) = 0;
+	
+};
+
+class Resistor : public Impedance_Device{
+	
+private:
+	double resistance;
+	
+public:
+	// constructor
+	Resistor(double r): resistance(r){}
+	
+	// getters
+	std::complex <double> get_impedance(double omega){
+		std::complex<double> impedance(resistance);
+		return impedance;
+	}
+	
+};
+
+class Capacitor : public Impedance_Device{
+	
+private:
+	double capacitance;
+	
+public:
+	// constructor
+	Capacitor(double c): capacitance(c){}
+	
+	// getters
+	std::complex<double> get_impedance(double omega){
+		std::complex<double> impedance(0, -1/(omega * capacitance));
+		return impedance;
+	}
+	
+};
+
+class Inductor : public Impedance_Device{
+	
+private:
+	double inductance;
+	
+public:
+	// constructor
+	Inductor(double i): inductance(i){}
+	
+	// getters
+	std::complex<double> get_impedance(double omega){
+		std::complex<double> impedance(0, omega * inductance);
+		return impedance;
+	}
+	
+};
 
 #endif /* IMPEDANCE_DEVICE_HPP_ */
